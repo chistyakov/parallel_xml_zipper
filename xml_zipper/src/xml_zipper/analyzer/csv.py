df24f5daa7e45900da947e8fd1b3768d99de74b3
Alexander Chistyakov <al.ol.chistyakov@gmail.com>
2018-08-24 23:17:04 +0300

    fix a typo

--- xml_zipper/src/xml_zipper/analyzer/csv.py
+++ xml_zipper/src/xml_zipper/analyzer/csv.py
@@ -11,11 +11,11 @@ def save_meta(dirpath: str) -> None:
     names_filename = os.path.join(dirpath, os.environ["NAMES_FILENAME"])
     with open(levels_filename, "w") as levels_f, open(names_filename, "w") as names_f:
         for meta_list in bunch_unzip(dirpath):
-            flash(meta_list, levels_f, names_f)
+            flush(meta_list, levels_f, names_f)
 
 
-def flash(meta_list: List[Meta], levels_file: TextIO, names_file: TextIO) -> None:
-    getLogger(__name__).info("flash data")
+def flush(meta_list: List[Meta], levels_file: TextIO, names_file: TextIO) -> None:
+    getLogger(__name__).info("flush data")
     for level_by_id, names_by_id in as_rows(meta_list):
         levels_file.write(f"{level_by_id}\n")
         names_file.write(f"{names_by_id}\n")
